9d3d44c62280d14e699e9a976ac9606f1d2f3e26
Edson <edsonmelo754@gmail.com>
2020-07-08 16:23:41 -0300

    Updates

---
+++ precache-manifest.5f1d327620968ff0f1a9fdc0958e52de.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "174f9e8f3cec93727a3d8994371edfce",
+    "url": "/project001dev/devfront/index.html"
+  },
+  {
+    "revision": "6b280b234dc41b9bdc47",
+    "url": "/project001dev/devfront/static/css/main.329633fa.chunk.css"
+  },
+  {
+    "revision": "e5ad4e5beff2cf26470d",
+    "url": "/project001dev/devfront/static/js/2.e77bfcf2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/project001dev/devfront/static/js/2.e77bfcf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b280b234dc41b9bdc47",
+    "url": "/project001dev/devfront/static/js/main.2eece56a.chunk.js"
+  },
+  {
+    "revision": "bf91fd9631c993596aa6",
+    "url": "/project001dev/devfront/static/js/runtime-main.7d5b83f8.js"
+  },
+  {
+    "revision": "4f9f8729ea86a947f733fd872a15cdf8",
+    "url": "/project001dev/devfront/static/media/avatar.4f9f8729.png"
+  },
+  {
+    "revision": "251c5fa1e8400b2dde0cf89721da8631",
+    "url": "/project001dev/devfront/static/media/curve.251c5fa1.svg"
+  },
+  {
+    "revision": "bf46290a2c7cefc59de7331f259cb448",
+    "url": "/project001dev/devfront/static/media/geometric-1732847_1920.bf46290a.jpg"
+  },
+  {
+    "revision": "decedce9052080c1c73df66c0c559c8b",
+    "url": "/project001dev/devfront/static/media/logotipo.decedce9.svg"
+  }
+]);
