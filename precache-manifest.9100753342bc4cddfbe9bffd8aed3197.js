b894e7ac0744018efcd668a21ca253bdd0d349dc
Edson <edsonmelo754@gmail.com>
2020-07-09 19:02:50 -0300

    Updates

---
+++ precache-manifest.9100753342bc4cddfbe9bffd8aed3197.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "936624f924c14e06566d0c6247d15939",
+    "url": "/project001dev/devfront/index.html"
+  },
+  {
+    "revision": "122b9888ac4a30b03000",
+    "url": "/project001dev/devfront/static/css/main.54e98971.chunk.css"
+  },
+  {
+    "revision": "2a83cd7113ee7edaf985",
+    "url": "/project001dev/devfront/static/js/2.fb0a4abf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/project001dev/devfront/static/js/2.fb0a4abf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "122b9888ac4a30b03000",
+    "url": "/project001dev/devfront/static/js/main.e0c4a96e.chunk.js"
+  },
+  {
+    "revision": "bf91fd9631c993596aa6",
+    "url": "/project001dev/devfront/static/js/runtime-main.7d5b83f8.js"
+  },
+  {
+    "revision": "4f9f8729ea86a947f733fd872a15cdf8",
+    "url": "/project001dev/devfront/static/media/avatar.4f9f8729.png"
+  },
+  {
+    "revision": "251c5fa1e8400b2dde0cf89721da8631",
+    "url": "/project001dev/devfront/static/media/curve.251c5fa1.svg"
+  },
+  {
+    "revision": "bf46290a2c7cefc59de7331f259cb448",
+    "url": "/project001dev/devfront/static/media/geometric-1732847_1920.bf46290a.jpg"
+  },
+  {
+    "revision": "ac61b407ac17acf027815137db7b4f98",
+    "url": "/project001dev/devfront/static/media/logo.ac61b407.png"
+  },
+  {
+    "revision": "decedce9052080c1c73df66c0c559c8b",
+    "url": "/project001dev/devfront/static/media/logotipo.decedce9.svg"
+  },
+  {
+    "revision": "c4b59f7e8b2f9cc1fd8efee1530bea58",
+    "url": "/project001dev/devfront/static/media/search.c4b59f7e.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/devfront/precache-manifest.2f96b6b317aabd48b77924224d61d165.js"
+  "/project001dev/devfront/precache-manifest.9100753342bc4cddfbe9bffd8aed3197.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/devfront/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/project001dev/devfront/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
